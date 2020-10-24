eaaaf6c0552b7cb0ae18efc6ac75d809359c98e8
amiisaikat <sbsaikatq413@gmail.com>
2020-10-24 23:49:28 +0530

    * sign up file changed. ** secret key added to __init__ file

--- api/signup.py
+++ api/signup.py
@@ -3,25 +3,30 @@ from flask import request, json
 from flask_jwt_extended import create_access_token
 
 
-@app.route('/user/signup', methods = "POST")
+@app.route('/user/signup', methods=["POST"])
 def user_signup():
     data = request.get_json(force=True)
-    user = users.find_one({'email': data['email']})
+    user = users.find_one({"email": data["email"]})
     if user is None:
-        hashedpw = bcrypt.generate_password_hash(data['password'])
+        hashed_pw = bcrypt.generate_password_hash(data["password"]).decode("UTF-8")
         user = {
-                'email': data['email'],
-                'password': hashedpw,
-                'username': data['username'],
-                'contact': data['contact']
+                "email": data["email"],
+                "password": hashed_pw,
+                "username": data['username'],
+                "contact": data['contact']
                 }
-        users.insert_one(user).inserted_id()
+        users.insert_one(user)
         response = json.jsonify({
-            "result": "ok",
-            "token": create_access_token(identity=data["email"])})
+                "result": {
+                            "username": user["username"],
+                            "email": user["email"]
+                            },
+                "token": create_access_token(identity=user["email"])
+            })
+
         return response, 200
     else:
         response = json.jsonify({
-            'results': 'User Already Exists, Try another email or forget password'
+            "results": "User Already Exists, Try another email or forget password"
         })
         return response
