992b645372f291330200644fb2448383fd829a82
amiisaikat <sbsaikatq413@gmail.com>
2020-10-24 22:34:26 +0530

    sign up changed

--- api/signup.py
+++ api/signup.py
@@ -1,6 +1,27 @@
-from api import app
+from api import app, users, bcrypt
+from flask import request, json
+from flask_jwt_extended import create_access_token
 
 
 @app.route('/user/signup')
 def user_signup():
-    return 'Hello'
+    data = request.get_json(force=True)
+    user = users.find_one({'email': data['email']})
+    if user is None:
+        hashedpw = bcrypt.generate_password_hash(data['password'])
+        user = {
+                'email': data['email'],
+                'password': hashedpw,
+                'username': data['username'],
+                'contact': data['contact']
+                }
+        users.insert_one(user).inserted_id()
+        response = json.jsonify({
+            "result": "ok",
+            "token": create_access_token(identity=data["email"])})
+        return response, 200
+    else:
+        response = json.jsonify({
+            'results': 'User Already Exists, Try another email or forget password'
+        })
+        return response
