31a86e175bb14c79223d55822caab7b8476ead29
sapta <saptarshisamanta97@gmail.com>
2020-10-24 15:50:18 +0530

    login

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, json
 from pymongo import MongoClient
+from werkzeug.security import safe_str_cmp
+from werkzeug.exceptions import HTTPException, BadRequest, InternalServerError, NotFound
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (
+    JWTManager
+)
 
 app = Flask(__name__)
+
+
+@app.errorhandler(InternalServerError)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description
+    })
+    response.content_type = "application/json"
+    return response
+
+
+@app.errorhandler(NotFound)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description
+    })
+    response.content_type = "application/json"
+    return response
+
+
 client = MongoClient('mongodb://localhost:27017/')
 db = client['e-commerce-beta']
 users = db['users']
 products = db['products']
+
+bcrypt = Bcrypt(app)
+
+jwt = JWTManager(app)
+
 from api import login
 from api import signup
--- api/login.py
+++ api/login.py
@@ -1,6 +1,29 @@
-from api import app,users
+from api import app, users, bcrypt
+from flask import request, json
+from flask_jwt_extended import create_access_token, jwt_optional, get_jwt_identity
 
 
-@app.route('/user/login')
+@app.route('/user/login', methods=["POST"])
+@jwt_optional
 def user_login():
-    return 'Hello World!'
+    current_user = get_jwt_identity()
+    if current_user:
+        return current_user
+    else:
+        data = request.get_json(force=True)
+        user = users.find_one({"email": data["email"]})
+        if user is None:
+            response = json.jsonify({
+                "result": "user_not_found"
+            })
+            return response
+        if bcrypt.check_password_hash(user["password"], data["password"]):
+            response = json.jsonify({
+                "result": "ok",
+                "token": create_access_token(identity=data["email"])
+            })
+            return response, 200
+        response = json.jsonify({
+            "result": "wrong_pass"
+        })
+        return response
